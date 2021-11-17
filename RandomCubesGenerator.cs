9920b1e11905774a9fdd2ad6f9e41239b2c5b947
Ryszard Sudujko <73278237+Yellow-Magic-Duck-of-War@users.noreply.github.com>
2021-11-17 19:45:13 +0100

    Aktualizacja zadania 4-1
    Uzupełniłem skrypt o ostatni punkt zadania - przydzielanie losowych materiałów do generowanych obiektów.

--- RandomCubesGenerator.cs
+++ RandomCubesGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,6 +36,9 @@ public class RandomCubesGenerator : MonoBehaviour
     // Lista zawiarająca losowe pozycje dla klocków:
     List<Vector3> pozycje = new List<Vector3>();
 
+    // Lista materiałów do przypisania:
+    public Material[] materialy = new Material[5];
+
     // Odstęp czasu między ręderowaniem obiektu:
     public float delay = 0.3f;
     int objectCounter = 0;
@@ -44,11 +47,13 @@ public class RandomCubesGenerator : MonoBehaviour
     {
         // Ustawienie pozycji platformy:
         platform.transform.position = new Vector3(platform_x_position, platform_y_position, platform_z_position);
+        
         // Ustawienie skali/wielkości platformy (platforma jest 10x większa od domyślnej kostki):
         platform.transform.localScale = new Vector3(platform_x_scale, platform_y_scale, platform_z_scale);
 
         // Punkt centralny spawnu - lokalna pozycja platformy:
         center = platform.transform.localPosition;
+        
         // Rozmiar spawnu - scala pratformy * 10:
         size = new Vector3(platform_x_scale * 10, 1, platform_z_scale * 10);
 
@@ -58,6 +63,7 @@ public class RandomCubesGenerator : MonoBehaviour
             // dodaj do listy pozycji nową pozycję opartą na losowych wartościach bazujących na skali/rozmiarze platformy: 
             this.pozycje.Add(center + new Vector3(UnityEngine.Random.Range((-size.x) / 2, (size.x) / 2), 5, UnityEngine.Random.Range((-size.z) / 2, (size.z) / 2)));
         }
+        
         // Dla każdego koordynatu w liście 'pozycje' ...
         foreach (Vector3 koordynaty in pozycje)
         {
@@ -81,17 +87,16 @@ public class RandomCubesGenerator : MonoBehaviour
         foreach (Vector3 pos in pozycje)
         {
             // zainicjuj obiekt `klocek` na pozycji `pozycje[objectCounter]`:
-            Instantiate(this.klocek, this.pozycje.ElementAt(this.objectCounter++), Quaternion.identity);
+            GameObject obiekt = Instantiate(this.klocek, this.pozycje.ElementAt(this.objectCounter++), Quaternion.identity);
+            
+            // Nadanie obiektowi losowego materiały z tablicy materiałów:
+            obiekt.GetComponent<MeshRenderer>().material = materialy[UnityEngine.Random.Range(0, materialy.Length)];
+            
             // Przeczekanie określonej ilości czasu aż funkja zostanie wywołana ponowanie (koślawe tłumaczenie...):
             yield return new WaitForSeconds(this.delay);
         }
         // Zatrzymanie coroutine:
         StopCoroutine(GenerujObiekt());
-    }
-
-    IEnumerator NadajMaterial()
-    {
-        yield return null;
     }
 
     private void OnDrawGizmosSelected()
