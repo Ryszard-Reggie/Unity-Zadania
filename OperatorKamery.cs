f829d8b4f8f88bf7509169a270636c68c1ada8b1
Ryszard Sudujko <73278237+Yellow-Magic-Duck-of-War@users.noreply.github.com>
2021-11-18 14:09:53 +0100

    Zadania 4-2 i 4-3
    * OperatorKamery.cs - skrypt odpowiada za operowanie kamerą w osi x i y.
    * PoruszanieCharakterem.cs - skrypt odpowiada za poruszanie obiektem po polu testowym.

---
+++ OperatorKamery.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperatorKamery : MonoBehaviour
+{
+    public float mouseSensiticity = 300f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensiticity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensiticity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ PoruszanieCharakterem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoruszanieCharakterem : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 5.0f;
+    public float gravity = -6.0f;
+    public float jump = 3.0f;
+
+    public Transform groundPoint;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundPoint.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jump * -2.0f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
